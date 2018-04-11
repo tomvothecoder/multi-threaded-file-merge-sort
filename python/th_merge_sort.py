33a85705fb5f919c75dd7ec8d354e18e19a502bf
Tom Vo <t_vo16@u.pacific.edu>
2018-04-11 10:14:35 -0700

    Using merge sort from geeksforgeeks.com as a tester before self-implementation

--- python/th_merge_sort.py
+++ python/th_merge_sort.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python3
- 
+##!/usr/bin/env python3
 import threading
 import time
 import sys
 
+lock = threading.RLock()
+
 """ Thread worker function """
 def thread_work(i):
-    print "sleeping 5 sec from thread %d" % i
-    time.sleep(5)
-    print "finished sleeping from thread %d" % i
+    lock.acquire()
+    print ("Thread lock acquired")
+    print("Sleeping 1 sec from thread: ", i)
+    time.sleep(1)
+    lock.release()
+    print ("Thread lock released")
+    print("Finished sleeping from thread: ", i)
 
 
 def main():
     thread_handles = []
-
     thread = int(sys.argv[1])
-
-    print "Threads selected: ", thread
+    print ("Threads selected: ", thread)
     
     for i in range(thread):
         t = threading.Thread(target=thread_work, args=(i, ))
