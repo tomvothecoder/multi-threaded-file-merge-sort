d3e80ff2798b011f73372207513b689df4b10efd
Tom Vo <t_vo16@u.pacific.edu>
2018-04-18 10:29:39 -0700

    Merge sort serial version complete - utilizing csv module and pandas

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -1,101 +1,98 @@
-##!/usr/bin/env python3
+#!/usr/bin/python3
+import sys
+import subprocess
 import csv
 import os
+import pandas as pd
 
-# Merges two subarrays of arr[].
-# First subarray is arr[l..m]
-# Second subarray is arr[m+1..r]
+def mergeSort(alist):
+    # print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
 
-def merge(arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r- m
- 
-    # create temp arrays
-    L = [0] * (n1)
-    R = [0] * (n2)
- 
-    # Copy data to temp arrays L[] and R[]
-    for i in range(0 , n1):
-        L[i] = arr[l + i]
- 
-    for j in range(0 , n2):
-        R[j] = arr[m + 1 + j]
- 
-    # Merge the temp arrays back into arr[l..r]
-    i = 0     # Initial index of first subarray
-    j = 0     # Initial index of second subarray
-    k = l     # Initial index of merged subarray
- 
-    while i < n1 and j < n2 :
-        if L[i] <= R[j]:
-            arr[k] = L[i]
-            i += 1
-        else:
-            arr[k] = R[j]
-            j += 1
-        k += 1
- 
-    # Copy the remaining elements of L[], if there
-    # are any
-    while i < n1:
-        arr[k] = L[i]
-        i += 1
-        k += 1
- 
-    # Copy the remaining elements of R[], if there
-    # are any
-    while j < n2:
-        arr[k] = R[j]
-        j += 1
-        k += 1
- 
-# l is for left index and r is right index of the
-# sub-array of arr to be sorted
-def mergeSort(arr,l,r):
-    if l < r:
- 
-        # Same as (l+r)/2, but avoids overflow for
-        # large l and h
-        m = (l+(r-1))/2
- 
-        # Sort first and second halves
-        mergeSort(arr, l, m)
-        mergeSort(arr, m+1, r)
-        merge(arr, l, m, r)
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i][1] < righthalf[j][1]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+    # print("Merging ", alist)
+
+
+def path_file(input_file):
+    path = os.path.abspath(input_file)
+    directory = os.path.dirname(path)
+
+    return directory
+
+
+def write_file(input_file, write_row):
+    with open(os.path.join('../dataset/', input_file), 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([write_row])
 
 
 def main():
     # Whole row
-    results = [] 
-    largest = 0
-    with open("dataset/data.csv") as csvfile:
-        reader = csv.reader(csvfile, delimiter = ",")
-        for row in reader: # each row is a list
+    results = []
+    # largest = 0
+
+    with open("../dataset/factbook2.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        next(reader)
+        next(reader)
+        for row in reader:  # each row is a list
             results.append(row)
             # country = row['country']
 
     n = len(results)
 
-    for i in range(n):
-        print("\nCurrent position: ", results[i][1])
-        if (int(results[i][1]) > largest):
-            largest = int(results[i][1])
-        print("Current largest", largest)
+    # for i in range(n):
+    #     print("\nCurrent position: ", results[i][1])
+    #     if (int(results[i][1]) > largest):
+    #         largest = int(results[i][1])
+    #     print("Current largest", largest)
 
-    print(results)
-    
-    mergeSort(arr,0,n-1)
-    
+    # print(results)
 
+    # mergeSort(results, 0, n - 1)
+    # mergeSort(results)
+    # print(results)
 
-"""   arr = [12, 11, 13, 5, 6, 7]
-    n = len(arr)
- 
-    mergeSort(arr,0,n-1)
-    print ("\n\nSorted array is")
-    
-    for i in range(n):
-        print ("%d" %arr[i]), """
+    print("Merge Sort - CSV File\n")
+
+    file_name = input("Enter CSV file name: ") + ".csv"
+    print("Saved as: ", file_name)
+    print("\tCSV file found at:" + path_file(file_name))
+
+    # write_file(file_name, results)
+
+    my_df = pd.DataFrame(results)
+    my_df.columns =["Country", "Death rate(deaths/1000 population)" ,"GDP"]
+    my_df.to_csv('../dataset/' + file_name, index=False)
+
+    print(my_df)
+
 
 
 if __name__ == "__main__":
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
 
+    largest = 0
